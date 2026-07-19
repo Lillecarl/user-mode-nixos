5559aa941cc8136bfc51ba7b431776c22d6ef088
lillecarl <git@lillecarl.com>
2026-07-20 01:14:37 +0200

    fix k8s image layer names: handle coredns/coredns path and image naming

--- modules/k8s-images.nix
+++ modules/k8s-images.nix
@@ -47,32 +47,32 @@ let
     installPhase = "mkdir -p $out; cp pause $out/";
   };
 
-  mkBinLayer = name: bin: pkgs.runCommand "${name}-layer" { } ''
-    mkdir -p $out
+  mkBinLayer = name: bin: pkgs.runCommand "${builtins.replaceStrings ["/"] ["-"] name}-layer" { } ''
+    mkdir -p $out/$(dirname ${name})
     cp ${bin} $out/${name}
   '';
 
   mkImage =
-    { name, bin, args ? [ ] }:
+    { imageName, bin, tag ? "v${version}", args ? [ ] }:
     dockerTools.buildLayeredImage {
-      name = "registry.k8s.io/${name}";
-      tag = "v${version}";
-      contents = [ (mkBinLayer name bin) ];
-      config.Entrypoint = [ "/${name}" ];
+      name = "registry.k8s.io/${imageName}";
+      inherit tag;
+      contents = [ (mkBinLayer imageName bin) ];
+      config.Entrypoint = [ "/${imageName}" ];
       config.Cmd = args;
     };
 
   images = [
-    (mkImage { name = "kube-apiserver"; bin = "${k8s}/bin/kube-apiserver"; })
-    (mkImage { name = "kube-controller-manager"; bin = "${k8s}/bin/kube-controller-manager"; })
-    (mkImage { name = "kube-scheduler"; bin = "${k8s}/bin/kube-scheduler"; })
-    (mkImage { name = "kube-proxy"; bin = "${k8s}/bin/kube-proxy"; })
+    (mkImage { imageName = "kube-apiserver"; bin = "${k8s}/bin/kube-apiserver"; })
+    (mkImage { imageName = "kube-controller-manager"; bin = "${k8s}/bin/kube-controller-manager"; })
+    (mkImage { imageName = "kube-scheduler"; bin = "${k8s}/bin/kube-scheduler"; })
+    (mkImage { imageName = "kube-proxy"; bin = "${k8s}/bin/kube-proxy"; })
     (mkImage {
-      name = "etcd";
+      imageName = "etcd";
       bin = "${pkgs.etcd}/bin/etcd";
       args = [ "--listen-client-urls=http://127.0.0.1:2379" "--advertise-client-urls=http://127.0.0.1:2379" ];
     })
-    (mkImage { name = "coredns/coredns"; bin = "${pkgs.coredns}/bin/coredns"; })
+    (mkImage { imageName = "coredns/coredns"; bin = "${pkgs.coredns}/bin/coredns"; })
     (dockerTools.buildLayeredImage {
       name = "registry.k8s.io/pause";
       tag = "3.10";
